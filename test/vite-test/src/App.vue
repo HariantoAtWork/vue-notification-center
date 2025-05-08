77b1c50e2b7fc0f8b1b04773f6fc727cc005e310
Harianto 🤖 <hariantoatwork@gmail.com>
2025-05-09 01:40:31 +0200

    Steps Notifcation

--- test/vite-test/src/App.vue
+++ test/vite-test/src/App.vue
@@ -46,6 +46,19 @@
         <button @click="showCustomPosition('bottomLeft')">Bottom Left</button>
       </div>
     </div>
+    <div class="test-section">
+      <h2>Elements</h2>
+      <div class="buttons">
+        <button @click="showElementProxy">Show Proxy</button>
+        <button @click="showElementObject">Show Object</button>
+      </div>
+    </div>
+    <div class="test-section">
+      <h2>Ask for Confirmation</h2>
+      <div class="buttons">
+        <button @click="step1">Ask Brother</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -53,6 +66,7 @@
   import createProxy from '@lib/createProxy'
   import { inject } from 'vue'
   const nc = inject('nc')
+  const notify = nc.addNotification
 
   const showInfo = () => {
     nc.addNotification({
@@ -103,9 +117,7 @@
   const showCustomDuration = duration => {
     nc.addNotification({
       title: 'Custom Duration',
-      message: `This notification will ${
-        duration === 0 ? 'not auto-close' : `close in ${duration / 1000} seconds`
-      }`,
+      message: `This notification will ${duration === 0 ? 'not auto-close' : `close in ${duration / 1000} seconds`}`,
       type: 'info',
       timeDuration: duration,
       showCloseButton: true,
@@ -119,6 +131,187 @@
       type: 'info',
       position,
     })
+  }
+
+  // Custom Elements: Proxy Object
+  const showElementProxy = () => {
+    // --- Proxy Object ---
+    const proxy = createProxy({
+      data: () => ({
+        text: 'Proxy',
+      }),
+      template: `As the world {{text}} turns<button @click="onDestroy">Destroy</button>`,
+      methods: {
+        onDestroy() {
+          if (typeof this.destroy === 'function') this.destroy()
+        },
+      },
+    })
+    proxy.destroy = () => {
+      notification.destroy()
+    }
+
+    const notification = notify({
+      elements: [proxy.$el],
+      position: 'topRight',
+      timeDuration: null,
+    })
+  }
+
+  // Custom Elements: Object
+  const showElementObject = () => {
+    const objectElement = {
+      template: `Everything is <div style="color: red;">{{text}}</div><p><button @click="onDestroy">Destroy</button></p>`,
+      data() {
+        return {
+          text: 'Text of Object Element',
+        }
+      },
+      methods: {
+        onDestroy() {
+          typeof notification.destroy === 'function' && notification.destroy()
+        },
+      },
+    }
+
+    const notification = notify({
+      elements: [objectElement],
+      position: 'topRight',
+      timeDuration: null,
+    })
+  }
+
+  // Ask for Confirmation
+  const askSomeoneAboutSomething = (option = {}, secondOption = {}) => {
+    const getDefaultOptions = (option = {}) => ({
+      elements: [objectElement],
+      title: 'Ask Brother About the Future',
+      message: 'What is the meaning of life?',
+      type: 'info',
+      timeDuration: null,
+      ...option,
+    })
+
+    const defaultOptions = getDefaultOptions(option)
+
+    const objectElement = {
+      template: `<h1 v-text="title" /><p v-text="message" /><p><button @click="onAskSomeone">Destroy</button></p>`,
+      data() {
+        return {
+          title: defaultOptions.title,
+          message: defaultOptions.message,
+        }
+      },
+      methods: {
+        onAskSomeone() {
+          typeof notification.destroy === 'function' && notification.destroy()
+        },
+      },
+    }
+
+    delete defaultOptions.title
+    delete defaultOptions.message
+
+    const notification = notify(defaultOptions)
+
+    return notification
+  }
+
+  const objectElement = ({ title, message, buttonText = 'Destroy', callback }) => ({
+    template: `<h1 v-text="title" /><p v-text="message" /><p><button v-if="!hideButton" @click="onAskSomeone">{{ buttonText }}</button></p>`,
+    data() {
+      return {
+        title,
+        message,
+        buttonText,
+        hideButton: false,
+      }
+    },
+    methods: {
+      onAskSomeone() {
+        typeof callback === 'function' && callback()
+        this.hideButton = true
+      },
+    },
+  })
+
+  const step1 = () => {
+    const n = notify({
+      timeDuration: null,
+      elements: [
+        objectElement({
+          title: 'Step 1',
+          message: 'Go to Step 2',
+          buttonText: 'Step 2',
+          callback() {
+            // n.destroy()
+
+            const n2 = step2()
+            n2.$on('destroy', n.destroy)
+          },
+        }),
+      ],
+    })
+
+    return n
+  }
+
+  const step2 = () => {
+    const n = notify({
+      position: 'topLeft',
+      timeDuration: null,
+      elements: [
+        objectElement({
+          title: 'Step 2',
+          message: 'Go to Step 3',
+          buttonText: 'Step 3',
+          callback() {
+            const n3 = step3()
+            n3.$on('destroy', n.destroy)
+          },
+        }),
+      ],
+    })
+
+    return n
+  }
+
+  const step3 = () => {
+    const n = notify({
+      position: 'topRight',
+      timeDuration: null,
+      elements: [
+        objectElement({
+          title: 'Step 3',
+          message: 'Go to Step 4',
+          buttonText: 'Step 4',
+          callback() {
+            const n4 = step4()
+            n4.$on('destroy', n.destroy)
+          },
+        }),
+      ],
+    })
+
+    return n
+  }
+
+  const step4 = () => {
+    const n = notify({
+      position: 'bottomRight',
+      timeDuration: null,
+      elements: [
+        objectElement({
+          title: 'Final Step',
+          message: 'Bye bye',
+          callback() {
+            n.destroy()
+          },
+        }),
+      ],
+    })
+
+    return n
   }
 </script>
 
